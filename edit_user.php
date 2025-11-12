f9ce6d3f2489db79a3a3ea2b59703a5f2a5d81e9
jlfernandez83 <jlfernandez83@gmail.com>
2025-11-12 14:27:32 +0100

    Actualización de usuario. Quedan tareas pendientes.

---
+++ edit_user.php
@@ -0,0 +1,45 @@
+<?php
+
+include_once('./libraries/functions.php');
+
+//Inicialización
+boot();
+
+//Lógica de negocio
+//Obtenemos id de la querystring
+$id = filter_input(INPUT_GET,'id', FILTER_VALIDATE_INT);
+
+if((isset($id))&&( $id !== false )){
+
+
+    if(isset($_POST['actualizar'])){
+    //Creamos usuario
+    $userData = filter_input_array(INPUT_POST,[
+        'nombre' => FILTER_DEFAULT,
+        'email' => FILTER_VALIDATE_EMAIL,
+        'rol' => FILTER_DEFAULT
+
+    ]);
+    if(!empty($userData)){
+        //Actualizo el usuario
+        //TODO Terminar esto
+    }
+    //Lee CSV
+    $usuarios = getDataFromCSV('./data/users.csv', 'id');
+    if (isset($usuarios[$id])){
+        $usuario = $usuarios[$id];
+        
+        
+    }else{
+        $usuario = null;
+    }
+
+}else{
+    header("Location: ./index_user.php"); 
+}
+
+
+//Lógica de presentación
+//Presenta el html a partir de los datos en el CSV
+include_once('./templates/edit_user.tpl.php');
+?>
